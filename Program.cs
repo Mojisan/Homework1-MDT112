715e576e9f6ffa56f16616bba0c54ecb41a70ceb
Jiramet <gozji0801@gmail.com>
2023-03-02 04:56:20 +0700

    Add file Program.cs : Input

--- Program.cs
+++ Program.cs
@@ -2,7 +2,14 @@
 namespace Homework1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            string agency, password;
+            Console.Write("Password : ");
+            password = Console.ReadLine();
+            Console.Write("Agency : ");
+            agency = Console.ReadLine();
+
+            Console.WriteLine(password);
+            Console.WriteLine(agency);
         }
     }
 }
