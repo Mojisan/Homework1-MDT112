7d4c10c3461a7095e045209d366d51d72a73b41a
Jiramet <gozji0801@gmail.com>
2023-03-02 05:07:51 +0700

    Add file Program.cs : FBI check

--- Program.cs
+++ Program.cs
@@ -10,32 +10,47 @@ namespace Homework1 {
                 Console.Write("Agency : ");
                 agency = Console.ReadLine();
                 switch(agency) {
-                case"CIA":
-                Console.WriteLine("Hello CIA");
-                if(((int)password[2] - 48) >= 6 && ((int)password[2] - 48) != 8) {
-                    if(((int)password[4] - 48) != 1 && ((int)password[4] - 48) != 3 && ((int)password[4] - 48) != 5) {
-                        if(((int)password[5] - 48) % 3 == 0) {
-                            Console.WriteLine("True");
+                    case"CIA":
+                    Console.WriteLine("Hello CIA");
+                    if(((int)password[2] - 48) >= 6 && ((int)password[2] - 48) != 8) {
+                        if(((int)password[4] - 48) != 1 && ((int)password[4] - 48) != 3 && ((int)password[4] - 48) != 5) {
+                            if(((int)password[5] - 48) % 3 == 0) {
+                                Console.WriteLine("True");
+                            } else {
+                                Console.WriteLine("False");
+                            }
                         } else {
-                            Console.WriteLine("False");
+                                Console.WriteLine("False");
                         }
                     } else {
+                        Console.WriteLine("False");
+                    }
+                    break;
+                    case"FBI":
+                    Console.WriteLine("Hello FBI");
+                    if(((int)password[0] - 48) >= 4 && ((int)password[0] - 48) <= 7) {
+                        if(((int)password[3] - 48) % 2 == 0 && ((int)password[3] - 48) != 6) {
+                           if(((int)password[1] - 48) % 2 != 0) {
+                            Console.WriteLine("True");
+                           } else {
+                            Console.WriteLine("False");
+                           }
+                        }
+                        else {
+                            Console.WriteLine("False");
+                        }
+                    }
+                    else {
                             Console.WriteLine("False");
                     }
-                } else {
-                    Console.WriteLine("False");
+                    break;
+                    case"NSA":
+                    Console.WriteLine("Hello NSA");
+                    break;
+                    default:
+                    Console.WriteLine("Error");
+                    break;
                 }
-                break;
-                case"FBI":
-                Console.WriteLine("Hello FBI");
-                break;
-                case"NSA":
-                Console.WriteLine("Hello NSA");
-                break;
-                default:
-                Console.WriteLine("Error");
-                break;
-            }
             } else {
                 Console.WriteLine("Error");
             }
